ccd03095b3721bd992fb0b6585b3c1072ffb23a8
xouabita <xouabita@gmail.com>
2015-06-18 15:37:46 +0800

    add webpack config to compile stylus

---
+++ web.coffee
@@ -0,0 +1,1 @@
+require "./assets/styles/main.styl"
--- webpack.config.js
+++ webpack.config.js
@@ -19,4 +19,28 @@ var backendConfig = {
   }
 }
 
-module.exports = backendConfig;
+var webConfig = {
+  entry: "./web.coffee",
+  output: {
+    path: "__build__/public/",
+    filename: "app.js"
+  },
+  resolve: {
+    moduleDirectories: ['node_modules']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.coffee$/,
+        loader: 'coffee-loader',
+        exclude: "node_modules"
+      },
+      {
+        test: /\.styl$/,
+        loader: 'style!css!stylus'
+      }
+    ]
+  }
+}
+
+module.exports = [backendConfig, webConfig];
